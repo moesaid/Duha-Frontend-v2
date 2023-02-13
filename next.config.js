2838773bbfbafefd870a1f187ed76acfbf2b5489
Omar Ali <omarxcoder@gmail.com>
2023-02-13 06:02:10 +0200

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    swcMinify: false,
+    trailingSlash: true,
+    env: {
+        // HOST
+        HOST_API_KEY: 'http://localhost:8000/api',
+        // FIREBASE
+        FIREBASE_API_KEY: 'AIzaSyDBbsCl6ClsQiLTJ7JQWXUV-xECvMnaX6c',
+        FIREBASE_AUTH_DOMAIN: 'test-project-79d8f.firebaseapp.com',
+        FIREBASE_PROJECT_ID: 'test-project-79d8f',
+        FIREBASE_STORAGE_BUCKET: 'test-project-79d8f.appspot.com',
+        FIREBASE_MESSAGING_SENDER_ID: '566617970623',
+        FIREBASE_APPID: '1:566617970623:web:9ffca6d7d52f264577730a',
+        FIREBASE_MEASUREMENT_ID: '',
+    },
+};
